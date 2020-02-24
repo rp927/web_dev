b9714a6903ce6bf8c058c51e9b9fa46755c79a7b
rp729 <rp729@yahoo.com>
2020-02-24 14:46:32 -0500

    flask

---
+++ flask/flaskblog.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Reuben Perry',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'February 24, 2020'
+    },
+    {
+        'author':'Jon Smith',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'February 25, 2020'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
