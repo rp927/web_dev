cb8a6f305da5f19b174793b5db75d99383f21601
rp729 <rp729@yahoo.com>
2020-02-25 15:50:03 -0500

    flask

--- flask/flaskblog.py
+++ flask/flaskblog.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e2f4f324642ceb654c9f03bca1064aa2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from models import User, Post
 
 posts = [
     {
@@ -28,14 +33,24 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register')
-def about():
+@app.route('/register', methods=['GET','POST'])
+def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html',title='Register',form=form)
 
-@app.route('/login')
-def about():
+@app.route('/login', methods=['GET','POST'])
+def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password','danger')
+
     return render_template('login.html',title='Login',form=form)
 
 if __name__ == '__main__':
